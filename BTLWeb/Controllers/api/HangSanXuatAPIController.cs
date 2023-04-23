5303e6248b06182235f6d513e325e0b7a05f5193
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-04-24 01:17:49 +0700

    done user api

--- BTLWeb/Controllers/api/HangSanXuatAPIController.cs
+++ BTLWeb/Controllers/api/HangSanXuatAPIController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BTLWeb.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BTLWeb.Controllers
@@ -7,6 +8,19 @@ namespace BTLWeb.Controllers
     [ApiController]
     public class HangSanXuatAPIController : ControllerBase
     {
+        QlbanMayAnhContext db = new QlbanMayAnhContext();
+        [HttpGet("hangsanxuat")]
+        public JsonResult GetAllProductByBrand([FromQuery] string mahangSx)
+        {
+            var lst = db.TDanhMucSps.Where(x => x.MaHangSx == mahangSx).OrderBy(x => x.TenSp).ToList();
+            return new JsonResult(lst);
+        }
 
+        [HttpGet]
+        public JsonResult GetAllBrand()
+        {
+            var lst = db.TDanhMucSps.ToList();
+            return new JsonResult(lst);
+        }
     }
 }
