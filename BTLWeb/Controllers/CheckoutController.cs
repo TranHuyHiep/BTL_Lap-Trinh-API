803fb89a91afcfabce58626bb0329c3e527dce7d
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-04-25 13:11:33 +0700

    update BE

---
+++ BTLWeb/Controllers/CheckoutController.cs
@@ -0,0 +1,25 @@
+﻿using BTLWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using X.PagedList;
+
+namespace BTLWeb.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        QlbanMayAnhContext db = new QlbanMayAnhContext();
+        public JsonResult DanhMucHoaDon(string maKhachHang)
+        {
+            int? page = 1;
+            int pageSize = 8;
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+            var lst = db.THoaDonBans
+                .Where(x => x.MaKhachHang.Equals(maKhachHang))
+                .OrderBy(x => x.MaHoaDon);
+
+            PagedList<THoaDonBan> pageList = new PagedList<THoaDonBan>(lst, pageNumber, pageSize);
+            return new JsonResult(pageList);
+        }
+    }
+}
